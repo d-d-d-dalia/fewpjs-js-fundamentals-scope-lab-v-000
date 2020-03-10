4b5a4edb8a54aa7d5a6fb4e7d193c12ade6d1aad
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-10 14:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  let animal = "cat"
+  
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let animal = "cat"
   return animal
 }
 
